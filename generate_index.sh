437c33e1990205f076cebe3bef76335c7e421de9
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-07-19 15:25:49 -0700

    init

---
+++ generate_index.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+die()
+{
+    echo "$@" 1>&2
+    exit 1
+}
+
+[ -f GIT_USER ] || die "file GIT_USER missing"
+git_user=$(cat GIT_USER)
+
+[ -f header.html ] || die "file header.html missing"
+[ -f footer.html ] || die "file footer.html missing"
+
+base_raw_url="https://raw.githubusercontent.com/$git_user"
+base_repo_url="https://github.com/$git_user"
+
+one_gallery()
+{
+    gallery_file="$1"
+    gallery_name=$(basename "$gallery_file")
+    repo_name=$(echo "$gallery_name" | sha1sum | head -c 10)
+    url="$base_raw_url/$repo_name/master"
+    dl_link="https://downgit.github.io/#/home?url=$base_repo_url/$repo_name/tree/master/orig"
+cat << EOF
+    <h2 id="$gallery_name"><a href="#$gallery_name">$gallery_name</a> <a href="$dl_link">💾</a></h2>
+    <div id="nanogallery2-$repo_name" data-nanogallery2 = '{
+         "thumbnailWidth": "auto",
+         "thumbnailHeight": 300,
+         "galleryDisplayTransition": "none",
+         "thumbnailDisplayTransition": "none",
+         "imageTransition": "slideAppear",
+         "viewerGallery": "none",
+         "viewerFullscreen": true,
+         "viewerTools":     {
+            "topLeft":    "playPauseButton",
+            "topRight":   "fullscreenButton, downloadButton, closeButton"
+         }
+    }' >
+EOF
+    for photo in $(cat "$gallery_file"); do
+        original=$url/orig/$photo
+        view=$url/view/$photo
+        mini=$url/mini/$photo
+cat << EOF
+    <a href="$view" data-ngthumb="$mini" data-ngdownloadurl="$original"></a>
+EOF
+    done
+cat << EOF
+    </div>
+EOF
+}
+
+generate_index()
+{
+    cdn="https://unpkg.com/nanogallery2@3.0.5/dist/"
+cat << EOF
+<!-- jQuery -->
+<script  type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<!-- nanogallery2 -->
+<link    href="$cdn/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
+<script  type="text/javascript" src="$cdn/jquery.nanogallery2.min.js"></script>
+<body style="background-color:lightgray">
+$(cat header.html)
+EOF
+
+    find galleries -type f | sort -r | while read g; do
+        one_gallery "$g"
+    done
+cat << EOF
+$(cat footer.html)
+</body>
+EOF
+}
+
+generate_index > index.html
---
+++ upload_gallery.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+die()
+{
+    echo "$@" 1>&2
+    exit 1
+}
+
+list_photos()
+{
+    folder="$1"
+    pushd "$folder" > /dev/null
+    find . | grep '.jpg' | sed -e 's#^\./##' | sort
+    popd > /dev/null
+}
+
+set_git_config()
+{
+    git config user.email 'mail'
+    git config user.name 'name'
+    # prevent git from trying to compress files on push
+    git config core.compression -1
+}
+
+convert_all_photos()
+{
+    for photo in $(find . | grep '.jpg'); do
+        dirname=$(dirname "$photo")
+        mkdir -p "orig/$dirname"
+        mkdir -p "view/$dirname"
+        mkdir -p "mini/$dirname"
+        original=orig/$photo
+        mv "$photo" "$original"
+        view=view/$photo
+        mini=mini/$photo
+        echo "convert -resize 2000x2000 -quality 90% $original $view"
+        echo "convert -resize 400x400 -quality 90% $original $mini"
+    done | parallel -j "$(nproc)" --bar
+}
+
+check_auth()
+{
+    [ -f GIT_USER ] || die "file GIT_USER missing"
+    git_user=$(cat GIT_USER)
+    gh auth status || die "need to authenticate using 'gh auth login' first"
+    auth_user=$(gh auth status |& grep "Logged in to github.com" | sed -e 's/.*github.com as //' -e 's/ (.*//')
+    [ "$auth_user" == "$git_user" ] || die "auth user $auth_user different from expected $git_user"
+}
+
+main()
+{
+    [ $# -eq 2 ] || die "usage: folder gallery_name"
+
+    folder="$1"; shift
+    gallery_name="$1";shift
+    repo_name=$(echo "$gallery_name" | sha1sum | head -c 10)
+
+    check_auth
+
+    tmp=$(mktemp -d)
+    trap "rm -rf $tmp" EXIT
+
+    cp -r "$folder" "$tmp/repo"
+    pushd "$tmp/repo"
+    convert_all_photos
+    git init
+    set_git_config
+    git add .
+    git commit -a -m 'init'
+    du -sh orig
+    du -sh view
+    du -sh mini
+    echo "push new repo \"$repo_name\" for gallery \"$gallery_name\""
+    gh repo create "$repo_name" --public --push --source ./
+    popd
+
+    set_git_config
+    mkdir -p galleries
+    list_photos "$folder" > galleries/"$gallery_name"
+    git add .
+    git commit -m "$gallery_name"
+}
+
+main "$@"
