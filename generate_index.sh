ed068f1e5fa53cbfe915dec4e5010a23816d5a16
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-07-19 19:24:48 -0700

    do not use nanogallery

--- generate_index.sh
+++ generate_index.sh
@@ -35,26 +35,13 @@ one_gallery()
     dl_link="https://downgit.github.io/#/home?url=$base_repo_url/$repo_name/tree/master/orig"
 cat << EOF
     <h2 id="$gallery_name"><a href="#$gallery_name">$gallery_name</a> <a href="$dl_link">💾</a> <a href="#top">⇪</a></h2>
-    <div id="nanogallery2-$repo_name" data-nanogallery2 = '{
-         "thumbnailWidth": "auto",
-         "thumbnailHeight": 300,
-         "galleryDisplayTransition": "none",
-         "thumbnailDisplayTransition": "none",
-         "imageTransition": "slideAppear",
-         "viewerGallery": "none",
-         "viewerFullscreen": true,
-         "viewerTools":     {
-            "topLeft":    "playPauseButton",
-            "topRight":   "fullscreenButton, downloadButton, closeButton"
-         }
-    }' >
 EOF
     for photo in $(cat "$gallery_file"); do
         original=$url/orig/$photo
         view=$url/view/$photo
         mini=$url/mini/$photo
 cat << EOF
-    <a href="$view" data-ngthumb="$mini" data-ngdownloadurl="$original"></a>
+    <a href="$view"><img loading="lazy" src="$mini" height=200px/></a>
 EOF
     done
 cat << EOF
@@ -64,14 +51,7 @@ EOF
 
 generate_index()
 {
-    cdn="https://unpkg.com/nanogallery2@3.0.5/dist/"
 cat << EOF
-<!-- jQuery -->
-<script  type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
-<!-- nanogallery2 -->
-<link    href="$cdn/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
-<script  type="text/javascript" src="$cdn/jquery.nanogallery2.min.js"></script>
 <body style="background-color:lightgray">
 $(cat header.html)
 <p id="top"></p>
